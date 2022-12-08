1b2bf0b80cc821a7e03685fafd39357cef608b91
VexWasHere <98134952+VexWasHere@users.noreply.github.com>
2022-12-08 03:21:57 +0000

    added FB

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-MpRt2Uf-JcdMx4Ux8MlfwF2PQn52H2Q",
+  authDomain: "venus-e20bf.firebaseapp.com",
+  projectId: "venus-e20bf",
+  storageBucket: "venus-e20bf.appspot.com",
+  messagingSenderId: "44812099216",
+  appId: "1:44812099216:web:b2b4983850af9efc534215",
+  measurementId: "G-281JTT5P56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
