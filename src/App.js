07f8e73fc66c1e2fe09ff1ed3234666126883bf2
VexWasHere <98134952+VexWasHere@users.noreply.github.com>
2022-12-09 20:34:21 +0000

    Updates to style and title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { useRef/*, useState */} from 'react';
+//import React, { Component } from 'react';
 
 function App() {
+  const queryRef = useRef(null);
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="topnav">
+  <a className="active" href="#home">Home</a>
+  <a href="#chat">Chat</a>
+  <a href="#support">Support</a>
+  <form>
+        <input name='query' ref={queryRef} type="text" placeholder='Search something awesome' onKeyDown={function(event){if(event.keyCode == 13){alert('query updated')}}}/>
+    </form>
+      </div>
+      <div name='home' className='content'>
         <h1>Hello World!</h1>
-      </header>
-    </div>
+      </div>
+      <div name='chat' className='content'>
+        <h1>Chat content</h1>
+      </div>
+</div>
   );
 }
 
