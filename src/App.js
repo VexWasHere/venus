688c6d62881c978e8494ce08a4ccb4c0da2e7eda
VexWasHere <98134952+VexWasHere@users.noreply.github.com>
2022-12-12 03:06:32 +0000

    restart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import "./App.css";
-import Tabs from "./Tabs";
+import './App.css';
 
 function App() {
-  return (
-    <div className="main">
-      <h1>Tabs Demo</h1>
-      <Tabs>
-        <div label="Gator">
-          See ya later, <em>Alligator</em>!
-        </div>
-        <div label="Croc">
-          After 'while, <em>Crocodile</em>!
-        </div>
-        <div label="Sarcosuchus">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
+  return(
+    <div className='App'>
     </div>
-  );
-};
+  )
+}
 export default App;
