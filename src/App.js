12b3644631e8b6aac78b4560675baae4c4f7d335
VexWasHere <98134952+VexWasHere@users.noreply.github.com>
2023-01-05 02:31:12 +0000

    style changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
-import styled, { css } from 'styled-components'
+import React from "react";
+import Tabs from "./Tabs";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <p>Hello World</p>
+      <Tabs>
+        <div label="My Home">
+          <div>
+            <h1>Nothing here!</h1>
+            <p>Why don't you add something new?</p>
+          </div>
+        </div>
+        <div label="My Business">
+          Working hard or hardly working!
+        </div>
+        <div label="Settings">
+          Nothing to see here!
+        </div>
+        <div label="Profile">
+          Coming soon!
+        </div>
+      </Tabs>
     </div>
   );
 }
-    
+
 export default App;
